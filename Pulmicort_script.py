cb6a905d475f92449f5d83c5a05f4a86dfd5423a
JensFugl <36481404+JensFugl@users.noreply.github.com>
2022-02-03 13:32:21 +0100

    user agent2

--- Pulmicort_script.py
+++ Pulmicort_script.py
@@ -27,7 +27,7 @@ options.add_argument("enable-automation")
 options.add_argument("--disable-infobars")
 options.add_argument("--disable-dev-shm-usage")
 
-#options.add_argument("user-agent=foo")
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0")
 driver = webdriver.Chrome(options=options)
 
 url = "https://www.medicinpriser.dk/default.aspx"
