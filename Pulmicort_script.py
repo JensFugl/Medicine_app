3cd7748884e594ee29152e40118101f90cc8bd27
JensFugl <36481404+JensFugl@users.noreply.github.com>
2022-02-03 12:59:33 +0100

    test

--- Pulmicort_script.py
+++ Pulmicort_script.py
@@ -21,11 +21,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 options = Options()
 options.add_argument("--headless")
-#options.add_argument("--disable-gpu")
-#options.add_argument("--no-sandbox")
-#options.add_argument("enable-automation")
-#options.add_argument("--disable-infobars")
-#options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(options=options)
 
 url = "https://www.medicinpriser.dk/default.aspx"
